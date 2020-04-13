f5aa76cfe3f8193b11f67f750728f815533daccf
Keven Pacheco Baraúna <kevenbarauna@gmail.com>
2020-04-13 17:23:12 -0300

    Pega IP do computador

---
+++ IP/IP/Program.cs
@@ -0,0 +1,26 @@
+﻿//using IdentityModel.Client;
+using System;
+using System.Net;
+
+namespace IP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("177.16.242.3");
+
+            Console.WriteLine("Seu IP da rede: IPV6");
+            string nome = Dns.GetHostName();
+            IPAddress[] ip = Dns.GetHostAddresses(nome);
+            Console.WriteLine(ip[1].ToString());
+
+            Console.WriteLine("NomePC");
+            string NomePc = Dns.GetHostName();
+            Console.WriteLine(NomePc.ToString());
+
+            // String IP = Request.UserHostAddress;
+        }
+    }
+}
